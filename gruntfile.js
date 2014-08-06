5427a54e046db36d1a5db050bb2d18b50facf571
Nika Tomadze <nika.tomadze@gmail.com>
2014-08-06 20:56:04 +0400

    minor fixes in syntax

--- gruntfile.js
+++ gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
 			},
 
 			build: ['Grunfile.js', 'scratch/js/**/*.js']
-	    },
+		},
 
 		uglify: {
 			options: {
--- scratch/js/main.js
+++ scratch/js/main.js
@@ -1,8 +1,17 @@
-/* 	
-	Nika Tomadze
-	This framework is written in a year before and it was implemented everyday. Today I decide to configure it for Github and make it public and everyone can download and use who'll need it. 
-*/
+/* 
+	Scratch Framework (https://github.com/nikoloza/Scratch)
+	Nika Tomadze (@nnikoloza)
+	nika.tomadze@gmail.com
 
+	-----
+	Use grunt for jshinting your javascript.
+	You need to install npm: 'grunt-contrib-jshint'
+	and 'grunt-contrib-jshint' for stylish error reportings
+
+	-----
+	Use grunt to minify js file.
+	You need to install npm: grunt-contrib-uglify
+*/
 
 requirejs.config({
 	paths: {
@@ -23,24 +32,63 @@ define(['jquery', 'fastclick'], function () {
 
 	$(function(){
 
-		// older ie
+		$('html').removeClass('no-js');
+
+		/* older ie
+		---------------------------------------------- */
 			function IE(v) {
 				var r = RegExp('msie' + (!isNaN(v) ? ('\\s' + v) : ''), 'i');
 				return r.test(navigator.userAgent);
 			}
 
-			if (IE(10) || IE(9) || IE(8) || IE(7)) { $('body').addClass('ie'); }
-			if (IE(10)) { $('body').addClass('ie10')};
-			if (IE(9)) { $('body').addClass('ie9')};
-			if (IE(8)) { $('body').addClass('ie8')};
-			if (IE(7)) { $('body').addClass('ie7')};
+			if (IE(10)) $('body').addClass('IE IE10');
+			if (IE(9)) $('body').addClass('IE IE9');
+			if (IE(8)) $('body').addClass('IE IE8');
+			if (IE(7)) $('body').addClass('IE IE7');
 
-		// ie 11
-			if (Object.hasOwnProperty.call(window, 'ActiveXObject') && !window.ActiveXObject) {
-				$('body').addClass('ie ie11');
-			};
+		/* ie 11
+		---------------------------------------------- */
+			if (Object.hasOwnProperty.call(window, 'ActiveXObject') && !window.ActiveXObject) $('body').addClass('IE IE11');
 
-		// something new comes up
+		/* if device
+		---------------------------------------------- */
+			var device = false;
+			if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) device = true;
+
+		/* which os
+		---------------------------------------------- */
+			var os = "UnknownOS";
+			if (navigator.appVersion.indexOf("Win")!=-1) os="Windows";
+			if (navigator.appVersion.indexOf("Mac")!=-1) os="MacOS";
+			if (navigator.appVersion.indexOf("X11")!=-1) os="UNIX";
+			if (navigator.appVersion.indexOf("Linux")!=-1) os="Linux";
+
+			$('body').addClass(os);
+
+		/* dom refference
+		---------------------------------------------- */
+			var doc 	 	= $(document),
+				win 		= $(window);
+
+				win.resize(function() {
+					win.w 	= win.width();
+					win.h 	= win.height();
+				}).resize();
+
+		/* helper methods
+		---------------------------------------------- */
+			$.fn.activate = function() {
+				$(this)
+					.addClass('active')
+					.siblings()
+					.removeClass('active');
+			}
+
+
+		/* hacking goes on
+		---------------------------------------------- */
+			// your code here
 
 	});
+
 });
